9e2bceab134eb1c3bbe092f903e4aa174438a096
sAmanzhol <Amanzhol1234567@gmail.com>
2019-05-19 17:21:07 +0600

    filter by genre added

--- app/src/main/java/com/example/kinokotlin/MainActivity.kt
+++ app/src/main/java/com/example/kinokotlin/MainActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import android.widget.AdapterView
 import android.widget.LinearLayout
 import android.widget.Toast
 import com.example.kinokotlin.adapter.MoviesItemAdapter
@@ -19,11 +21,16 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.ArrayList
+import android.widget.ArrayAdapter
+import com.example.kinokotlin.R.string.genre
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mToggle: ActionBarDrawerToggle
     private var genres: List<Genre> = ArrayList()
+    private var genreId: Int = 28
+    private var movieType: String = "most_popular"
 
     val APIKey = "88e00a6ff7f2cd56c1f07c582cf84355"
     internal var service = RetrofitClient.retrofitInstance.create(JsonPlaceHolderApi::class.java)
@@ -46,40 +53,77 @@ class MainActivity : AppCompatActivity() {
         mToggle.syncState()
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
+        spinnerGenre?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("not implemented")
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                var c: Call<Movie>? = null
+                var ge: Genre = spinnerGenre.selectedItem as Genre
+                genreId = ge.id
+                if (movieType.equals("most_popular")) {
+                    c = service.getPopularMovie(APIKey)
+                    requestMovieList(c)
+                } else if (movieType.equals("top_rated")) {
+                    c = service.getTopRatedMovie(APIKey)
+                    requestMovieList(c)
+                } else if (movieType.equals("upcoming")) {
+                    c = service.getUpcomingMovie(APIKey)
+                    requestMovieList(c)
+                } else if (movieType.equals("latest")) {
+                    c = service.getNowPlayingMovie(APIKey)
+                    requestMovieList(c)
+                } else if (movieType.equals("now_playing")) {
+                    c = service.getNowPlayingMovie(APIKey)
+                    requestMovieList(c)
+                }
+
+
+            }
+
+        }
+
         nav_view.setNavigationItemSelectedListener {
+            genreId = 0
             var call: Call<Movie>? = null
             when (it.itemId) {
                 R.id.most_popular -> {
                     call = service.getPopularMovie(APIKey)
                     requestMovieList(call)
+                    movieType = "most_popular"
                     activity_main.closeDrawers()
                     true
                 }
                 R.id.top_rated -> {
                     call = service.getTopRatedMovie(APIKey)
                     requestMovieList(call)
+                    movieType = "top_rated"
                     activity_main.closeDrawers()
                     true
                 }
                 R.id.upcoming -> {
                     call = service.getUpcomingMovie(APIKey)
                     requestMovieList(call)
+                    movieType = "upcoming"
                     activity_main.closeDrawers()
                     true
                 }
                 R.id.latest -> {
                     call = service.getNowPlayingMovie(APIKey)
                     requestMovieList(call)
+                    movieType = "latest"
                     activity_main.closeDrawers()
                     true
                 }
                 R.id.now_playing -> {
                     call = service.getNowPlayingMovie(APIKey)
                     requestMovieList(call)
+                    movieType = "now_playing"
                     activity_main.closeDrawers()
                     true
                 }
-                R.id.login-> {
+                R.id.login -> {
                     PreferenceUtils.saveLogged(this, false)
                     startActivity(Intent(this, LoginActivity::class.java))
                     true
@@ -98,8 +142,10 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<Movie>, t: Throwable) {
-                Toast.makeText(baseContext, "Unable to load movies",
-                    Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    baseContext, "Unable to load movies",
+                    Toast.LENGTH_SHORT
+                ).show()
                 t.printStackTrace()
             }
         })
@@ -111,21 +157,45 @@ class MainActivity : AppCompatActivity() {
         call.enqueue(object : Callback<PreGenre> {
             override fun onResponse(call: Call<PreGenre>, response: Response<PreGenre>) {
                 genres = response.body()?.genres!!
+                populateGenreList(genres)
             }
 
             override fun onFailure(call: Call<PreGenre>, t: Throwable) {
-                Toast.makeText(baseContext, "Unable to load genres",
-                    Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    baseContext, "Unable to load genres",
+                    Toast.LENGTH_SHORT
+                ).show()
                 t.printStackTrace()
             }
         })
     }
 
     private fun generateMovieList(results: List<Result>, genreList: List<Genre>) {
+        val filteredResults: MutableList<Result> =  mutableListOf<Result>()
 
-        val adapter = MoviesItemAdapter(this, results, genreList)
+        if (genreId != 0) {
+            for (i in results.indices) {
+                if (results[i].genreIds!!.contains(genreId)) {
+                    filteredResults.add(results[i])
+                }
+            }
+
+        } else {
+            filteredResults.addAll(results)
+        }
+        println(genreId)
+        println(filteredResults)
+        val adapter = MoviesItemAdapter(this, filteredResults, genreList)
         movies.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
         movies.adapter = adapter
+
+    }
+
+    private fun populateGenreList(genreList: List<Genre>) {
+        val dataAdapter = ArrayAdapter<Genre>(this, android.R.layout.simple_spinner_item, genreList)
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerGenre.adapter = dataAdapter
 
     }
 }
--- app/src/main/java/com/example/kinokotlin/adapter/MoviesItemAdapter.kt
+++ app/src/main/java/com/example/kinokotlin/adapter/MoviesItemAdapter.kt
@@ -32,35 +32,35 @@ class MoviesItemAdapter(private val context: Context,
 
     inner class MoviesItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
         fun bindMovieItem(movie: Result) {
+                var movieGenre = ""
+                val genreMap = HashMap<Int, String>()
+                for (i in mGenreList.indices) {
+                    genreMap[mGenreList[i].id] = mGenreList[i].name
+                }
 
-            var movieGenre = ""
-            val genreMap = HashMap<Int, String>()
-            for (i in mGenreList.indices) {
-                genreMap[mGenreList[i].id] = mGenreList[i].name
-            }
+                Picasso.get().load(IMAGE_URL + movie.posterPath).into(view.movie_poster_view)
+                view.read_more_button.tag = movie.id
+                view.movie_title.text = movie.title
+                view.movie_vote_average.text = movie.voteAverage.toString()
+                view.movie_release_date.text = movie.releaseDate
 
-            Picasso.get().load(IMAGE_URL + movie.posterPath).into(view.movie_poster_view)
-            view.read_more_button.tag = movie.id
-            view.movie_title.text = movie.title
-            view.movie_vote_average.text = movie.voteAverage.toString()
-            view.movie_release_date.text = movie.releaseDate
+                for (id in movie.genreIds!!) {
+                    if (!movieGenre.isEmpty()) {
+                        movieGenre = movieGenre + "," + genreMap[id]
+                    } else {
+                        movieGenre += genreMap[id]
+                    }
+                }
 
-            for (id in movie.genreIds!!) {
-                if (!movieGenre.isEmpty()) {
-                    movieGenre = movieGenre + "," + genreMap[id]
-                } else {
-                    movieGenre += genreMap[id]
+                movieGenre = movieGenre.replace(",".toRegex(), "\n")
+                view.movie_genre.text = movieGenre
+
+                view.read_more_button.setOnClickListener {
+                    val intent = Intent(context, MovieDetailsActivity::class.java)
+                    intent.putExtra("MOVIE_ID", view.read_more_button.tag as Int)
+                    context.startActivity(intent)
                 }
             }
 
-            movieGenre = movieGenre.replace(",".toRegex(), "\n")
-            view.movie_genre.text = movieGenre
-
-            view.read_more_button.setOnClickListener {
-                val intent = Intent(context, MovieDetailsActivity::class.java)
-                intent.putExtra("MOVIE_ID", view.read_more_button.tag as Int)
-                context.startActivity(intent)
-            }
-        }
     }
 }
