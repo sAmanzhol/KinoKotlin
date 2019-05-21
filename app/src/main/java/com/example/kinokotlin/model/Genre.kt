f443e88e72652a7e1c6f10c27c4bce4a3e13cf44
sAmanzhol <Amanzhol1234567@gmail.com>
2019-05-21 10:22:01 +0600

    fix toString genre

--- app/src/main/java/com/example/kinokotlin/model/Genre.kt
+++ app/src/main/java/com/example/kinokotlin/model/Genre.kt
@@ -5,4 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class Genre (
     @SerializedName("id") var id: Int,
     @SerializedName("name") var name: String
-)
+
+){
+    override fun toString(): String = name
+}
